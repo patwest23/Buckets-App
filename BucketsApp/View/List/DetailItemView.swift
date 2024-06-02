7a6eb827cf9247eb29f9b1d08b19075588818ec2
Patrick <patrickwesterkamp@gmail.com>
2024-06-01 21:23:27 -0500

    - added a DetailItemView - modified the ListView and ItemModel and the ImagePickerModel for the new DetailItemView - revised the addButton to be larger and have a haptic feedback and to only add to list if the next item isn't empty

---
+++ BucketsApp/View/List/DetailItemView.swift
@@ -0,0 +1,18 @@
+//
+//  DetailItemView.swift
+//  BucketsApp
+//
+//  Created by Patrick Westerkamp on 6/1/24.
+//
+
+import SwiftUI
+
+struct DetailItemView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    DetailItemView()
+}
