a76daee2b1fa6697110b74ff6d09b454469bb358
Patrick <patrickwesterkamp@gmail.com>
2024-12-29 14:55:11 -0600

    - starting to integrate Firestore database for the app

---
+++ BucketsApp/ViewModel/Extensions.swift
@@ -0,0 +1,8 @@
+//
+//  Extensions.swift
+//  BucketsApp
+//
+//  Created by Patrick Westerkamp on 12/13/24.
+//
+
+import Foundation
---
+++ BucketsApp/ViewModel/UserViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  UserViewModel.swift
+//  BucketsApp
+//
+//  Created by Patrick Westerkamp on 12/29/24.
+//
+
+import Foundation
