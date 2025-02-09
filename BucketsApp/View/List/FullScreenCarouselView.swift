d9d15df9b804db4b6a05376a07bac1651bcffc0e
Patrick <patrickwesterkamp@gmail.com>
2025-02-09 14:46:37 -0600

    - changes to the ItemRowView and simplified design for now...

---
+++ BucketsApp/View/List/FullScreenCarouselView.swift
@@ -0,0 +1,18 @@
+//
+//  FullScreenCarouselView.swift
+//  BucketsApp
+//
+//  Created by Patrick Westerkamp on 2/9/25.
+//
+
+import SwiftUI
+
+struct FullScreenCarouselView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    FullScreenCarouselView()
+}
