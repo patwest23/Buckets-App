40e2abc412960efd66b87f408f3caccc5625e98e
Patrick <patrickwesterkamp@gmail.com>
2025-05-17 15:55:09 -0500

    - added followerslistview and followinglistview

---
+++ BucketsApp/View/User/FollowingListView.swift
@@ -0,0 +1,18 @@
+//
+//  FollowingListView.swift
+//  BucketsApp
+//
+//  Created by Patrick Westerkamp on 5/17/25.
+//
+
+import SwiftUI
+
+struct FollowingListView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    FollowingListView()
+}
