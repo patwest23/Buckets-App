3af619f52d0a1fc7e4643d5f306b5b7164690765
Patrick <patrickwesterkamp@gmail.com>
2025-05-25 22:12:12 -0500

    - still working on making the image picker, detailitemview, and list view show and save the images to Firestore

---
+++ BucketsApp/View/Social/FollowerView.swift
@@ -0,0 +1,18 @@
+//
+//  FollowerView.swift
+//  BucketsApp
+//
+//  Created by Patrick Westerkamp on 5/25/25.
+//
+
+import SwiftUI
+
+struct FollowerView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    FollowerView()
+}
---
+++ BucketsApp/View/Social/FollowingView.swift
@@ -0,0 +1,18 @@
+//
+//  FollowingView.swift
+//  BucketsApp
+//
+//  Created by Patrick Westerkamp on 5/25/25.
+//
+
+import SwiftUI
+
+struct FollowingView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    FollowingView()
+}
