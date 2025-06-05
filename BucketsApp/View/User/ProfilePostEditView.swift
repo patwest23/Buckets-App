35e99b4718c1711b5f4ba6f1135a8f16db7e5144
Patrick <patrickwesterkamp@gmail.com>
2025-06-04 21:00:23 -0500

    - add profileposteditview and profileposteditrow so that the posts can show in the profileview

---
+++ BucketsApp/View/User/ProfilePostEditView.swift
@@ -0,0 +1,18 @@
+//
+//  ProfilePostEditView.swift
+//  BucketsApp
+//
+//  Created by Patrick Westerkamp on 6/4/25.
+//
+
+import SwiftUI
+
+struct ProfilePostEditView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ProfilePostEditView()
+}
---
+++ BucketsApp/View/User/ProfilePostRowView.swift
@@ -0,0 +1,18 @@
+//
+//  ProfilePostRowView.swift
+//  BucketsApp
+//
+//  Created by Patrick Westerkamp on 6/4/25.
+//
+
+import SwiftUI
+
+struct ProfilePostRowView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ProfilePostRowView()
+}
