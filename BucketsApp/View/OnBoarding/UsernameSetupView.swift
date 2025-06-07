ca13763173516cababe30c1d0fff54f29832856a
Patrick <patrickwesterkamp@gmail.com>
2025-06-07 14:54:48 -0500

    - updated the profilepostrowview and added google sign in

---
+++ BucketsApp/View/OnBoarding/UsernameSetupView.swift
@@ -0,0 +1,18 @@
+//
+//  UsernameSetupView.swift
+//  BucketsApp
+//
+//  Created by Patrick Westerkamp on 6/7/25.
+//
+
+import SwiftUI
+
+struct UsernameSetupView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    UsernameSetupView()
+}
