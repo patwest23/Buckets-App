7ee0723e7cb71ed114203988c70fb016242ddc66
patwest23 <40448795+patwest23@users.noreply.github.com>
2025-11-22 14:32:25 -0600

    Harden ItemModel decoding defaults

--- BucketsApp/Model/ItemModel.swift
+++ BucketsApp/Model/ItemModel.swift
@@ -59,7 +59,28 @@ struct ItemModel: Codable, Identifiable, Hashable {
     /// Usernames (prefixed with "@") that this item has been shared with.
     var sharedWithUsernames: [String]
 
-    // MARK: - Initializer
+    enum CodingKeys: String, CodingKey {
+        case id
+        case userId
+        case name
+        case description
+        case url
+        case dueDate
+        case hasDueTime
+        case tags
+        case location
+        case flagged
+        case priority
+        case completed
+        case orderIndex
+        case creationDate
+        case imageUrls
+        case sharedImageUrls
+        case sharedWithUsernames
+    }
+
+    // MARK: - Initializers
+
     init(
         id: UUID = UUID(),
         userId: String,
@@ -96,6 +117,49 @@ struct ItemModel: Codable, Identifiable, Hashable {
         self.imageUrls = imageUrls
         self.sharedImageUrls = sharedImageUrls
         self.sharedWithUsernames = sharedWithUsernames
+    }
+
+    init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+
+        id = try container.decodeIfPresent(UUID.self, forKey: .id) ?? UUID()
+        userId = try container.decodeIfPresent(String.self, forKey: .userId) ?? ""
+        name = try container.decodeIfPresent(String.self, forKey: .name) ?? ""
+        description = try container.decodeIfPresent(String.self, forKey: .description)
+        url = try container.decodeIfPresent(String.self, forKey: .url)
+        dueDate = try container.decodeIfPresent(Date.self, forKey: .dueDate)
+        hasDueTime = try container.decodeIfPresent(Bool.self, forKey: .hasDueTime) ?? false
+        tags = try container.decodeIfPresent([Tag].self, forKey: .tags)
+        location = try container.decodeIfPresent(Location.self, forKey: .location)
+        flagged = try container.decodeIfPresent(Bool.self, forKey: .flagged) ?? false
+        priority = try container.decodeIfPresent(Priority.self, forKey: .priority) ?? .none
+        completed = try container.decodeIfPresent(Bool.self, forKey: .completed) ?? false
+        orderIndex = try container.decodeIfPresent(Int.self, forKey: .orderIndex) ?? 0
+        creationDate = try container.decodeIfPresent(Date.self, forKey: .creationDate) ?? Date()
+        imageUrls = try container.decodeIfPresent([String].self, forKey: .imageUrls) ?? []
+        sharedImageUrls = try container.decodeIfPresent([String].self, forKey: .sharedImageUrls) ?? []
+        sharedWithUsernames = try container.decodeIfPresent([String].self, forKey: .sharedWithUsernames) ?? []
+    }
+
+    func encode(to encoder: Encoder) throws {
+        var container = encoder.container(keyedBy: CodingKeys.self)
+        try container.encode(id, forKey: .id)
+        try container.encode(userId, forKey: .userId)
+        try container.encode(name, forKey: .name)
+        try container.encode(description, forKey: .description)
+        try container.encode(url, forKey: .url)
+        try container.encode(dueDate, forKey: .dueDate)
+        try container.encode(hasDueTime, forKey: .hasDueTime)
+        try container.encode(tags, forKey: .tags)
+        try container.encode(location, forKey: .location)
+        try container.encode(flagged, forKey: .flagged)
+        try container.encode(priority, forKey: .priority)
+        try container.encode(completed, forKey: .completed)
+        try container.encode(orderIndex, forKey: .orderIndex)
+        try container.encode(creationDate, forKey: .creationDate)
+        try container.encode(imageUrls, forKey: .imageUrls)
+        try container.encode(sharedImageUrls, forKey: .sharedImageUrls)
+        try container.encode(sharedWithUsernames, forKey: .sharedWithUsernames)
     }
 
     // MARK: - Computed Properties
